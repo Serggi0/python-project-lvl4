aaec2cdbcbeac24978e264121eef0c32a792a82a
Serggi0 <serve4mia@gmail.com>
2021-09-28 20:29:07 +0300

    Add app and set DJANGO_SETTINGS_MODULE

---
+++ task_manager/asgi.py
@@ -0,0 +1,19 @@
+"""
+ASGI config for task_manager project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+# ! изменения после команды:
+# ! $ export DJANGO_SETTINGS_MODULE=hello_django.settings
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
+
+application = get_asgi_application()
