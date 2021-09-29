46c117f5a7c64f7c951d0d9eaf7e1155bddf81ec
Serggi0 <serve4mia@gmail.com>
2021-09-29 11:50:27 +0300

    Add Procfile and requirements.txt

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv() # ! 
+load_dotenv()  # !
 env_path = '.env'
 load_dotenv(dotenv_path=env_path)
 
