322b0f2ca25bb2238637e94189e486abe1b2b608
Serggi0 <serve4mia@gmail.com>
2021-11-29 14:43:00 +0300

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=False)
 DATABASES['default'].update(db_from_env)
 
 
