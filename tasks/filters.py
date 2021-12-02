c06a07bdcc0596d24eca6873e0771522aef9deb5
Serggi0 <serve4mia@gmail.com>
2021-12-02 13:13:12 +0300

    fix

--- tasks/filters.py
+++ tasks/filters.py
@@ -11,17 +11,16 @@ class TaskFilter(filters.FilterSet):
     Объявление фильтруемыми поля fields. См. class TasksView
     '''
     own_tasks = filters.BooleanFilter(
-        field_name='author',
         label=_('Only your own tasks'),
+        field_name='author',
         method='filter_own_tasks',
         widget=CheckboxInput
     )
 
     def filter_own_tasks(self, queryset, name, value):
         if value:
-            if self.request.user.pk:
-                return queryset.filter(author__pk=self.request.user.pk)
-        return queryset
+            return queryset.filter(author=self.request.user).order_by('pk')
+        return queryset.order_by('pk')
 
     label_ = filters.ModelChoiceFilter(
         queryset=Label.objects.all(),
@@ -31,6 +30,12 @@ class TaskFilter(filters.FilterSet):
     class Meta:
         model = Task
         fields = ['status', 'executor', 'label_', 'own_tasks']
+        filter_overrides = {
+            filters.BooleanFilter: {
+                'filter_class': filters.BooleanFilter,
+                'extra': lambda f: {'widget': CheckboxInput},
+            },
+        }
 
 
 # class TaskFilter(filters.FilterSet):
@@ -56,3 +61,12 @@ class TaskFilter(filters.FilterSet):
 #     class Meta:
 #         model = Task
 #         fields = ['status', 'executor', 'label_', 'own_tasks']
+
+    # def filter_own_tasks(self, queryset, name, value):
+    #     if value:
+    #         return queryset.filter(author__pk=self.request.user.pk)
+    #     return queryset
+
+    # def filter_own_tasks(self, queryset, name, value):
+    #     lookup = '__'.join([name, 'author'])
+    #     return queryset.filter(**{lookup: False})
