c236f38ae29b256950978aaffa0e5cea9beef472
Serggi0 <serve4mia@gmail.com>
2022-01-04 19:25:25 +0300

    change tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -41,34 +41,15 @@ class TaskTestCase(TestCase):
 
     def test_create_task(self):
         self.client.login(username='ivanich', password='123test098')
-        l2 = Label(name='label_name2')
-        l2.save()
-        s2 = Status(name='status_name2')
-        s2.save()
-        self.task = Task(
-            name='task_create',
-            author=self.user1,
-            executor=self.user2,
-            status=Status.objects.get(pk=2),
+        response = self.client.post(
+            reverse('tasks:create_task'),
+            {
+                'name': 'task_create',
+                'status': Status.objects.get(name='status_name').id,
+            }
         )
-        self.task.save()
-        self.task.labels.add(l2)
-        self.assertIn(self.task, Task.objects.all())
-        self.assertIn('task_create', Task.objects.get(pk='2').name)
-
-    def test_task_view_logout(self):
-        self.client.logout()
-        response = self.client.get(
-            '/logout/'
-        )
-        response = self.client.get(reverse('tasks:tasks'))
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/login/')
-
-    def test_task_no_view(self):
-        response = self.client.get(reverse('tasks:tasks'))
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/login/')
+        self.assertTrue(Task.objects.get(name='task_create'))
 
     def test_update_task(self):
         self.client.login(username='masha', password='098test!@#')
@@ -91,3 +72,17 @@ class TaskTestCase(TestCase):
         )
         self.assertEqual(response.status_code, 302)
         self.assertFalse(Task.objects.filter(pk=1))
+
+    def test_task_view_logout(self):
+        self.client.logout()
+        response = self.client.get(
+            '/logout/'
+        )
+        response = self.client.get(reverse('tasks:tasks'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/login/')
+
+    def test_task_no_view(self):
+        response = self.client.get(reverse('tasks:tasks'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/login/')
